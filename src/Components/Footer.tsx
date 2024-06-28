b16c4a40cbfa8060611916c412310b2468e38cfd
Ronan LE MEILLAT <ronan@mail.com>
2024-06-28 18:08:37 +0200

    correct copyright

--- src/Components/Footer.tsx
+++ src/Components/Footer.tsx
@@ -13,8 +13,12 @@ function Footer(): JSX.Element {
           Custom QR Code Styling.
         </a>
         {' - '}{' '}
-        <a target='_blank' rel='noopener noreferrer' href='https://github.com/awran5/custom-qr-code-styling'>
-          (c) 2024 Les Ailes du Mont-Blanc
+        <a target='_blank' rel='noopener noreferrer' href='https://lesailesdumontblanc.com'>
+        © 2024 Les Ailes du Mont-Blanc
+        </a>
+        &nbsp;et&nbsp;
+        <a target='_blank' rel='noopener noreferrer' href='https://sctg.eu.org'>
+        Ronan
         </a>
       </p>
     </footer>
