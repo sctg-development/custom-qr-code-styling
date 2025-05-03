0745605e41a148ee59624d2267c29cbf6a64af43
æLtorio <aeltorio@universidadatlantico.org>
2025-05-03 15:21:37 +0200

    fix: download in any format chore: update to latest dependencies

--- src/Components/Download.tsx
+++ src/Components/Download.tsx
@@ -1,30 +1,30 @@
 /**
  * Copyright (c) 2021 - awran5
- * Copyright (c) 2024 - Ronan Le Meillat
+ * Copyright (c) 2024-2025 - Ronan Le Meillat
  * Provided under the MIT License. See License file for details.
  */
 import React, { ReactElement, useCallback, useContext } from 'react'
 import { AppContext } from '../Context'
-import { browserUtils } from '@liquid-js/qr-code-styling'
+import { browserUtils, FileExtension } from '@liquid-js/qr-code-styling'
 const Download = (): ReactElement => {
   const { qrCode } = useContext(AppContext)
 
   
-  const handleDownload = useCallback((ext: string) => browserUtils?.download(qrCode, ext as any), [qrCode])
+  const handleDownload = useCallback((extension: FileExtension) => browserUtils?.download(qrCode, { extension, name:"QRCode" }), [qrCode])
 
   return (
     <div className='d-flex align-items-center pt-4'>
       <div className='pe-3 fw-bold text-uppercase'>Download</div>
-      <button type='button' className='btn btn-outline-primary me-1' onClick={() => handleDownload('jpg')}>
+      <button type='button' className='btn btn-outline-primary me-1' onClick={() => handleDownload(FileExtension.jpeg)}>
         .JPG
       </button>
-      <button type='button' className='btn btn-outline-primary me-1' onClick={() => handleDownload('png')}>
+      <button type='button' className='btn btn-outline-primary me-1' onClick={() => handleDownload(FileExtension.png)}>
         .PNG
       </button>
-      <button type='button' className='btn btn-outline-primary me-1' onClick={() => handleDownload('webp')}>
+      <button type='button' className='btn btn-outline-primary me-1' onClick={() => handleDownload(FileExtension.webp)}>
         .WEBP
       </button>
-      <button type='button' className='btn btn-outline-primary' onClick={() => handleDownload('svg')}>
+      <button type='button' className='btn btn-outline-primary' onClick={() => handleDownload(FileExtension.svg)}>
         .SVG
       </button>
     </div>
--- vite.config.ts
+++ vite.config.ts
@@ -1,11 +1,12 @@
 import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
 import tsconfigPaths from 'vite-tsconfig-paths';
+import { githubPagesSpa } from "@sctg/vite-plugin-github-pages-spa";
 import { resolve } from 'path';
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react(), tsconfigPaths()],
+  plugins: [react(), tsconfigPaths(), githubPagesSpa()],
   resolve: {
     alias: {
       src: resolve(__dirname, 'src')
